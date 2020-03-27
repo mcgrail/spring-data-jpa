487bfc4bbc7faf2e3d7935d595a44fd7c19eea54
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 22:44:04 -0500

    Fixed syntax issue

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -14,7 +14,7 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 
-	protected Customer()ds {}
+	protected Customer() {}
 
 	public Customer(String firstName, String lastName) {
 		this.firstName = firstName;

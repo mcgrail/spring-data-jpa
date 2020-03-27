cb7d592c3f86b18707f5290cb7a0541b4a2c97e1
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 22:42:12 -0500

    Added a breaking change to test the CI GitHub action

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -14,7 +14,7 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 
-	protected Customer()d {}
+	protected Customer()ds {}
 
 	public Customer(String firstName, String lastName) {
 		this.firstName = firstName;

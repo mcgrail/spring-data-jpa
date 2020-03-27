0b61b7d86f19026c9e50e620f761e0b47f4d7267
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 21:05:43 -0500

    Removing bin files, added a unit test, and updating .gitignore

--- src/test/java/com/example/demo/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/demo/SpringDataJpaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private CustomerRepository customers;
+
 	@Test
-	void contextLoads() {
+	public void testFindByLastName() {
+		Customer customer = new Customer("first", "last");
+		entityManager.persist(customer);
+
+		List<Customer> findByLastName = customers.findByLastName(customer.getLastName());
+
+		assertThat(findByLastName).extracting(Customer::getLastName).containsOnly(customer.getLastName());
 	}
 
 }

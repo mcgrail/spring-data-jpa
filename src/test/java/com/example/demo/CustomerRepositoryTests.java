dfd4b460d59862eb62436acc9e72090747706074
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 22:14:22 -0500

    Fixed unit test

--- src/test/java/com/example/demo/CustomerRepositoryTests.java
+++ src/test/java/com/example/demo/CustomerRepositoryTests.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringDataJpaApplicationTests {
+@DataJpaTest
+class CustomerRepositoryTests {
 
 	@Autowired
 	private TestEntityManager entityManager;

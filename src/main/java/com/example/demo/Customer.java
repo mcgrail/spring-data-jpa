a4060fb3a4f2ffe26f35b7382553898053424b96
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 22:39:51 -0500

    Temporarily adding a breaking change to test Java CI badge for GitHub CI action

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -14,7 +14,7 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 
-	protected Customer() {}
+	protected Customer()d {}
 
 	public Customer(String firstName, String lastName) {
 		this.firstName = firstName;

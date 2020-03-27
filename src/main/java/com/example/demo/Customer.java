e1155002257731b3e87ff14f8f0918d9344c3039
mcgrail <mcgrail@ryans-mbp.lan>
2020-03-26 22:54:35 -0500

    Fixing compile error

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -17,7 +17,7 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 
-	protected Customer()d {}
+	protected Customer() {}
 
 	public Customer(String firstName, String lastName) {
 		this.firstName = firstName;
